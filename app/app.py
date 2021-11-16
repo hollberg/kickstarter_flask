ed859a70111bb611c23721571199617173a7e80e
Mitchell Hollberg <mhollberg@gmail.com>
2021-11-16 09:34:33 -0500

    new template

--- app/app.py
+++ app/app.py
@@ -1,4 +1,11 @@
+"""
+app.py - main app for the Kickstarter evaluator tool
+"""
+
+# *** IMPORTS ***
+from os import getenv
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 def create_app():
@@ -6,8 +13,19 @@ def create_app():
 
     @app.route('/')
     def hello_world():  # put application's code here
-        return 'Hello World!'
+        return 'Hello World!' + getenv('DATABASE_URL')
     return app
+
+
+    @app.route('/db')
+    def load_db():
+        """
+
+        :return:
+        """
+
+
+
 
 if __name__ == '__main__':
     app = create_app()

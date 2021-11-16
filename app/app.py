9b285f3d0b8490538dd19a3811f24704ee4302cd
Mitchell Hollberg <mhollberg@gmail.com>
2021-11-16 11:01:23 -0500

    Added database model

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app.py - main app for the Kickstarter evaluator tool
 """
 
 # *** IMPORTS ***
-from .models import DB
+from .models import DB, Kickstarter
 from os import getenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -12,20 +12,29 @@ from flask_sqlalchemy import SQLAlchemy
 def create_app():
     app = Flask(__name__)
 
+    # Give our APP access to our database
+    DB.init_app(app)
+
     @app.route('/')
     def hello_world():  # put application's code here
-        return 'Hello World!' + getenv('DATABASE_URL')
+        return 'Hello World!'   # + getenv('DATABASE_URL')
 
 
     @app.route('/db')
     def load_db():
         """
-
+        Empty and repopulate the database with tables defined in models.py
         :return:
         """
         DB.drop_all()
         DB.create_all()
-        return 'Database updated!'
+        new_ks = Kickstarter(id=1, name='test')
+        DB.session.add(new_ks)
+        ks = Kickstarter.query.all()
+        ks_string = str(ks[0])
+        DB.session.commit()
+        # DB.table('Kickstarter')
+        return 'Database updated!' + ks_string
 
     return app
 
--- app/models.py
+++ app/models.py
@@ -11,25 +11,28 @@ DB = SQLAlchemy()
 
 
 # *** Define Models/Tables ***
-class Kickstarters(DB.Model):
+class Kickstarter(DB.Model):
     """
     Defines the "Kickstarters" table with SQLAlchemy
     """
     id = DB.Column(DB.BigInteger, primary_key=True)
-    name = DB.Column(DB.String)
-    blurb = DB.Column(DB.String)
-    goal = DB.Column(DB.Float)
-    campaign_duration = DB.Column(DB.Float)
-    current_currency = DB.Column(DB.String)
-    fx_rate = DB.Column(DB.Float)
-    static_usd_rate = DB.Column(DB.Float)
-    outcome = DB.Column(DB.Boolean)
-    days_to_success = DB.Column(DB.Float)
-    city = DB.Column(DB.String)
-    state = DB.Column(DB.String)
-    country = DB.Column(DB.String)
-    category = DB.Column(DB.String)
-    subcategory = DB.Column(DB.String)
-    location = DB.Column(DB.String)
-    latitude = DB.Column(DB.Float)
-    longitude = DB.Column(DB.Float)
+    name = DB.Column(DB.String, nullable=False)
+    blurb = DB.Column(DB.String, nullable=True)
+    goal = DB.Column(DB.Float, nullable=True)
+    campaign_duration = DB.Column(DB.Float, nullable=True)
+    current_currency = DB.Column(DB.String, nullable=True)
+    fx_rate = DB.Column(DB.Float, nullable=True)
+    static_usd_rate = DB.Column(DB.Float, nullable=True)
+    outcome = DB.Column(DB.Boolean, nullable=True)
+    days_to_success = DB.Column(DB.Float, nullable=True)
+    city = DB.Column(DB.String, nullable=True)
+    state = DB.Column(DB.String, nullable=True)
+    country = DB.Column(DB.String, nullable=True)
+    category = DB.Column(DB.String, nullable=True)
+    subcategory = DB.Column(DB.String, nullable=True)
+    location = DB.Column(DB.String, nullable=True)
+    latitude = DB.Column(DB.Float, nullable=True)
+    longitude = DB.Column(DB.Float, nullable=True)
+
+    def __repr__(self):
+        return f'Kickstarter: name - {self.name}'
